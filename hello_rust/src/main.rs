0af60c610a1cfff9cb041dbe69d24b7360898495
nob0dj <dongxuan1863@gmail.com>
2023-10-05 00:23:50 +0900

    Add: [hello_rust] 다양한 주석
    - //
    - /* */
    - ///

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,13 @@
+/// api doc comments
+
 fn main() {
     println!("Hello, world!");
+    // 한줄 주석
+    /* 
+        여러줄 주석
+    */
+    let _x/* : i64 */= 10; 
+    // 선언한 변수를 사용하지 않으면 warning: unused variable: `x`
+    // 변수 접두사 _를 사용하면 무시하라는 뜻
+    // println!("{}", x)
 }
