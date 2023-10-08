88776a4aeba8bafbd9013c161c7b4be0e8a0ccef
nob0dj <dongxuan1863@gmail.com>
2023-10-09 07:28:09 +0900

    Add: [types] string

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,8 +1,30 @@
+use std::mem::size_of;
+
 fn main() {
     // type_integer();
     // type_char();
-    casting();
+    // casting();
+    type_string();
 }
+
+/// string
+/// string.len() byte수
+/// string.chars().count() 글자수
+fn type_string() {
+    // use 구문 사용하기
+    println!("Size of a char: {}", size_of::<char>()); // 4 bytes
+    println!("Size of a char: {}", std::mem::size_of::<char>()); // 4 bytes
+    println!("Size of string containing 'a': {}", "a".len()); // .len() gives the size of the string in bytes
+    println!("Size of string containing 'ß': {}", "ß".len());
+    println!("Size of string containing '国': {}", "国".len()); 
+    println!("Size of string containing '𓅱': {}", "𓅱".len());
+
+    let slice = "Hello!"; // 6bytes, 6글자
+    println!("Slice is {} bytes and also {} characters.", slice.len(), slice.chars().count());
+    let slice2 = "안녕!"; // 7bytes(한글 각 3bytes), 3글자
+    println!("Slice2 is {} bytes but only {} characters.", slice2.len(), slice2.chars().count());
+}
+
 /// simple type changing using 'as'
 fn casting() {
     let a : u8 = 10;
