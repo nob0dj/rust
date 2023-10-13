ccfc326bdf4e64b9a2c8828ce9efba46bf67425b
nob0dj <dongxuan1863@gmail.com>
2023-10-13 19:58:46 +0900

    Add: [basics] empty_tuple
    - semicolon
    - empty_tuple
    - {:?} Debug print

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    macro_println();
+    // macro_println();
+    semicolon();
+}
+
+fn semicolon() {
+    // println!("{}", empty_tuple());  // help: the trait `std::fmt::Display` is not implemented for `()`
+    
+    // empty_tuple처럼 Display할수 없는 경우, Debug print {:?}할 수 있다.
+    println!("{:?}", empty_tuple()) // ()
+}
+
+/// rust 함수의 void 즉, return 값이 없는 경우 empty_tuple을 반환한다.
+/// -> ()
+fn empty_tuple() -> (){
+    // 8; // 리턴구문이라면 ;이 없어야 한다. ;이 있다면 return값이 아닌 statement.
 }
 
 /// macro 
